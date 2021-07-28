ecb4be9055aee79e4c458b399392db895bc3948c
Lange, Dr. Herbert <herbert.lange@uni-hamburg.de>
2021-07-28 14:28:05 +0200

    transform the imdi checker into a generic abstract checker and implement it for both imdi and childes

---
+++ src/main/java/de/uni_hamburg/corpora/validation/GenericMetadataChecker.java
@@ -0,0 +1,287 @@
+package de.uni_hamburg.corpora.validation;
+
+//import com.fasterxml.jackson.annotation.JsonAutoDetect;
+//import com.fasterxml.jackson.annotation.PropertyAccessor;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.databind.SerializationFeature;
+
+import com.opencsv.bean.CsvToBeanBuilder;
+import de.uni_hamburg.corpora.*;
+import org.exmaralda.partitureditor.fsm.FSMException;
+import org.exmaralda.partitureditor.jexmaralda.JexmaraldaException;
+import org.jdom.JDOMException;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.xpath.XPathExpressionException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.security.NoSuchAlgorithmException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author bba1792 Dr. Herbert Lange
+ * @version 20210728
+ *
+ * Abstract Checker for the generic metadata
+ */
+abstract class GenericMetadataChecker extends Checker implements CorpusFunction {
+
+    /**
+     * Default constructor without parameter, not providing fixing options
+     */
+    public GenericMetadataChecker() {
+        this(false);
+    }
+
+    /**
+     * Default constructor with optional fixing option
+     * @param hasfixingoption the fixing option
+     */
+    public GenericMetadataChecker(boolean hasfixingoption) {
+        super(hasfixingoption);
+    }
+
+    // The local logger that can be used for debugging
+    final Logger logger = Logger.getLogger(this.getClass().toString());
+
+    // Flag to see if the checker is properly set up
+    boolean setUp = false;
+
+    // Data structure representing all metadata criteria
+    List<GenericMetadataCriterion> criteria;
+
+    /**
+     * Checker function for a single document in a corpus
+     * @param cd the corpus file
+     * @param fix the fixing option
+     * @return a detailed report of the checker
+     * @throws NoSuchAlgorithmException inherited
+     * @throws ClassNotFoundException inherited
+     * @throws FSMException inherited
+     * @throws URISyntaxException inherited
+     * @throws SAXException inherited
+     * @throws IOException inherited
+     * @throws ParserConfigurationException inherited
+     * @throws JexmaraldaException inherited
+     * @throws TransformerException inherited
+     * @throws XPathExpressionException inherited
+     * @throws JDOMException inherited
+     */
+    @Override
+    public Report function(CorpusData cd, Boolean fix) throws NoSuchAlgorithmException, ClassNotFoundException, FSMException, URISyntaxException, SAXException, IOException, ParserConfigurationException, JexmaraldaException, TransformerException, XPathExpressionException, JDOMException {
+        Report report = new Report();
+        // Only work if properly set up
+        if (setUp) {
+            for (GenericMetadataCriterion c : criteria) {
+                // Collect all values of properties that match one of the locators
+                 ArrayList<String> values = new ArrayList<>();
+                for (String locator : c.locator) {
+                    // Ignore any property without locator
+                    if (!locator.equals("N/A")) {
+                        // get the values based on the locator
+                        report.merge(getValuesForLocator(cd, locator, values));
+                    }
+                }
+                // Check if we have at least lower bound elements (if lower bounds are defined, i.e. not N/A)
+                if (c.bounds.lower != GenericMetadataCriterion.Bounds.EBounds.NA && !c.locator.contains("N/A") &&
+                        GenericMetadataCriterion.compareToBounds(values.size(), c.bounds.lower) < 0)
+                    report.addCritical(function, cd, "Less than " + GenericMetadataCriterion
+                            .Bounds.toString(c.bounds.lower) + " occurrences of " + c.name + " found: " + values.size());
+                // Check if we have at most upper bound elements (if upper bounds are defined, i.e. not N/A)
+                if (c.bounds.upper != GenericMetadataCriterion.Bounds.EBounds.NA && !c.locator.contains("N/A") &&
+                        GenericMetadataCriterion.compareToBounds(values.size(), c.bounds.upper) > 0)
+                    report.addCritical(function, cd, "More than " + GenericMetadataCriterion
+                            .Bounds.toString(c.bounds.upper) + " occurrences of " + c.name + " found: " + values.size());
+                // Now check all the results
+                for (String value : values) {
+                    // Get the value of the node, either from an element or an attribute
+                    // DEBUG check the path for a property
+//                    if (c.name.equalsIgnoreCase("PublicationYear")) {
+//                        String path = getPathForElement((Element) o);
+//                        logger.log(Level.INFO, "PublicationYear: " + path);
+//                    }
+                    // Check if we can parse the value as one of the valid types
+                    boolean parsable = false;
+                    for (Optional<String> t : c.type) {
+                        // No type, we don't have to parse it
+                        if (!t.isPresent())
+                            parsable = true;
+                        else {
+                            // String is always valid
+                            if (t.get().equalsIgnoreCase("string")) {
+                                parsable = true;
+                            }
+                            // Testing an URI/URL
+                            else if (t.get().equalsIgnoreCase("uri")) {
+                                try {
+                                    // Just check if it is a uri
+                                    new URI(value);
+                                    parsable = true;
+                                } catch (URISyntaxException e) {
+                                    report.addCritical(function, cd, "Invalid URI syntax " + e);
+                                    break ;
+                                }
+                                // Also try it as a URL
+                                URL url = null;
+                                try {
+                                    // Handle URIs can start with hdl: and can be resolved using a proxy
+                                    if (value.startsWith("hdl:")) {
+                                        url = new URL(value.replace("hdl:", "https://hdl.handle.net/"));
+
+                                    }
+                                    // DOI URIs can start with hdl: and can be resolved using a proxy
+                                    else if (value.startsWith("doi:")) {
+                                        url = new URL(value.replace("doi:", "https://doi.org/"));
+                                    }
+                                    // HTTP URIs are URLs
+                                    else if (value.startsWith("http")) {
+                                        url = new URL(value);
+                                    }
+                                } catch (MalformedURLException e) {
+                                    report.addWarning(function, cd, "Malformed URL " + e);
+                                    break ;
+                                }
+                                // If we succeed in creating a URL object we can try to connect
+                                /*if (url != null) {
+                                    try {
+                                        TimeUnit.MILLISECONDS.sleep(500);
+                                        HttpURLConnection con = ((HttpURLConnection) url.openConnection());
+                                        con.setRequestMethod("GET");
+                                        int code = con.getResponseCode();
+                                        if (code != 200)
+                                            report.addWarning(function, cd, "Error connecting to url " + url + ": " + code);
+                                    } catch (IOException | InterruptedException e) {
+                                        report.addWarning(function, cd, "Exception when connecting to url " + e);
+                                    }
+
+                                }*/
+
+                            }
+                            // Check date
+                            else if (t.get().equalsIgnoreCase("date")) {
+                                // Try various date formats
+                                try {
+                                    new SimpleDateFormat("yyyy-MM-dd").parse(value);
+                                    parsable = true;
+                                } catch (ParseException e) {
+                                    parsable = false;
+                                }
+                                if (!parsable)
+                                    try {
+                                        new SimpleDateFormat("yyyy-MM").parse(value);
+                                        parsable = true;
+                                    } catch (ParseException e) {
+                                        parsable = false;
+                                    }
+                                if (!parsable)
+                                    try {
+                                        new SimpleDateFormat("yyyy").parse(value);
+                                        parsable = true;
+                                    } catch (ParseException e) {
+                                        parsable = false;
+                                    }
+                            }
+                        }
+                    }
+                    // Error if everything failed
+                    if (!parsable)
+                        report.addCritical(function, cd, "No valid type given for value " + value +
+                                " (Attempted " + c.type + ")");
+                }
+            }
+        } else
+            report.addCritical(function, cd, "No criteria file loaded");
+        return report;
+    }
+
+
+    /**
+     * Checker function for a corpus
+     * @param c the corpus
+     * @param fix the fixing parameter
+     * @return detailed report of the checker
+     * @throws NoSuchAlgorithmException inherited
+     * @throws ClassNotFoundException inherited
+     * @throws FSMException inherited
+     * @throws URISyntaxException inherited
+     * @throws SAXException inherited
+     * @throws IOException inherited
+     * @throws ParserConfigurationException inherited
+     * @throws JexmaraldaException inherited
+     * @throws TransformerException inherited
+     * @throws XPathExpressionException inherited
+     * @throws JDOMException inherited
+     */
+    @Override
+    public Report function(Corpus c, Boolean fix) throws NoSuchAlgorithmException, ClassNotFoundException, FSMException, URISyntaxException, SAXException, IOException, ParserConfigurationException, JexmaraldaException, TransformerException, XPathExpressionException, JDOMException {
+        Report report = new Report();
+        if (setUp) {
+            // Apply function for each supported file
+            Collection usable = this.getIsUsableFor();
+            for (CorpusData cdata : c.getCorpusData()) {
+                if (usable.contains(cdata.getClass())) {
+                    report.merge(function(cdata, fix));
+                }
+            }
+        } else
+            report.addCritical(function, "No criteria file loaded");
+        return report;
+    }
+
+    /**
+     * Function to get a collection of values based on a locator
+     *
+     * @param locator the locator for the values
+     * @return the values determiend by the locator
+     */
+    protected abstract Report getValuesForLocator(CorpusData cd, String locator, Collection<String> values);
+
+    /**
+     * Function to load a criteria file and set up the checker
+     * @param filename the criteria as a csv file to be loaded
+     */
+    public void setCriteriaFile(String filename) {
+        try {
+            // DEBUG
+            //logger.log(Level.INFO, "Filename: " + filename);
+            //logger.log(Level.INFO, "Reading CSV " + filename);
+            // Read CSV file
+            criteria = new CsvToBeanBuilder<GenericMetadataCriterion>(new FileReader(filename))
+                    .withType(GenericMetadataCriterion.class)
+                    .withSkipLines(1) // skip header
+                    .build()
+                    .parse();
+            // DEBUG write criteria to log
+            /*for (GenericMetadataCriterion c : criteria) {
+                logger.log(Level.INFO,"Name: " + c.name + ", Bounds: " + c.bounds.lower + " - " + c.bounds.upper +
+                        ", Type: " + c.type + ", XPath: " + c.locator);
+            }*/
+            // DEBUG Write criteria to json file
+            /*ObjectMapper mapper = new ObjectMapper();
+            // Allows serialization even when getters are missing
+            mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+            mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+            try {
+                FileWriter fw = new FileWriter("/tmp/imdi.json");
+                fw.write(mapper.writeValueAsString(criteria));
+                fw.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }*/
+            // Checker is properly set up
+            setUp = true;
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Encountered exception when loading criteria ", e);
+        }
+    }
+}
---
+++ src/main/java/de/uni_hamburg/corpora/validation/GenericMetadataCriterion.java
@@ -0,0 +1,129 @@
+package de.uni_hamburg.corpora.validation;
+
+import com.opencsv.bean.*;
+import com.opencsv.exceptions.CsvConstraintViolationException;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Class representing the metadata criteria including property name, lower and upper bounds for cardinality and locator
+ * to find the property in a metadata file
+ */
+public class GenericMetadataCriterion {
+
+    public static class Bounds {
+        public enum EBounds {B0, B1, Unbounded, NA}
+
+        EBounds upper;
+        EBounds lower;
+
+        public Bounds() {}
+
+        public Bounds(EBounds lower, EBounds upper) {
+            this.upper = upper ;
+            this.lower = lower ;
+        }
+
+        public static String toString(EBounds b) {
+            String s ;
+            switch (b) {
+                case B0:
+                    s = "0";
+                    break;
+                case B1:
+                    s = "1";
+                    break;
+                case Unbounded:
+                    s = "unbounded";
+                    break;
+                default:
+                    s = "N/A";
+                    break;
+            }
+            return s ;
+        }
+    }
+
+    public static class ToBounds extends AbstractBeanField {
+
+        public ToBounds() {}
+
+        @Override
+        protected Object convert(String value) throws CsvDataTypeMismatchException, CsvConstraintViolationException {
+            return new Bounds(toLower(value),toUpper(value));
+        }
+
+        private Bounds.EBounds toLower(String value) {
+            if (value.startsWith("0")) {
+                return Bounds.EBounds.B0 ;
+            }
+            else if (value.startsWith("1")) {
+                return Bounds.EBounds.B1;
+            }
+            else {
+                return Bounds.EBounds.NA;
+            }
+        }
+
+        private Bounds.EBounds toUpper(String value) {
+            if (value.endsWith("0")) {
+                return Bounds.EBounds.B0 ;
+            }
+            else if (value.endsWith("1")) {
+                return Bounds.EBounds.B1 ;
+            }
+            else if (value.endsWith("unbounded")) {
+                return Bounds.EBounds.Unbounded ;
+            }
+            else {
+                return Bounds.EBounds.NA ;
+            }
+        }
+    }
+
+    public static class ToClass extends AbstractBeanField {
+
+        @Override
+        protected Object convert(String value) throws CsvDataTypeMismatchException, CsvConstraintViolationException {
+            return Arrays.stream(value.split("\\s+OR\\s+")).map(this::toOptional).collect(Collectors.toList()) ;
+        }
+
+        private Optional<String> toOptional(String value) {
+            if (value.equalsIgnoreCase("string") || value.equalsIgnoreCase("uri") ||
+                    value.equalsIgnoreCase("date"))
+                return Optional.of(value) ;
+            else
+                return Optional.empty() ;
+        }
+    }
+
+    public static int compareToBounds(int i, Bounds.EBounds b) {
+        int j = 0 ; // same as Bounds.B0
+        // Update j depending on b
+        if (b == Bounds.EBounds.B1)
+            j = 1 ;
+        // Unbounded is always larger than any number
+        else if (b == Bounds.EBounds.Unbounded)
+            j = Integer.MAX_VALUE ;
+        // Everything else is smaller
+        else if (b == Bounds.EBounds.NA)
+            return Integer.MIN_VALUE ;
+        // Use the built-in comparison
+        return Integer.compare(i,j);
+
+    }
+
+    public GenericMetadataCriterion() {}
+
+    @CsvBindByPosition(position = 0)
+    String name ;
+    @CsvCustomBindByPosition(position = 1, converter = ToBounds.class)
+    Bounds bounds ;
+    @CsvCustomBindByPosition(position = 2, converter = ToClass.class)
+    List<Optional<String>> type ;
+    @CsvBindAndSplitByPosition(position = 3, elementType = String.class, splitOn = "\\s+OR\\s+")
+    List<String> locator;
+}
+
