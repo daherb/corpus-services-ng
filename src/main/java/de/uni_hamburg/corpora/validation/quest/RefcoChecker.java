eb7d4ba5485afc94c76bec4ee91ad6f91b5da611
Lange, Dr. Herbert <herbert.lange@uni-hamburg.de>
2021-12-10 20:01:56 +0100

    add comment for method

--- src/main/java/de/uni_hamburg/corpora/validation/quest/RefcoChecker.java
+++ src/main/java/de/uni_hamburg/corpora/validation/quest/RefcoChecker.java
@@ -2160,6 +2160,12 @@ public class RefcoChecker extends Checker implements CorpusFunction {
         return params;
     }
 
+
+    /**
+     * Returns all text contained in a DOM subtree given by the root element
+     * @param e the root of the subtree
+     * @return all text contained in the subtree
+     */
     public static String showAllText(Element e) {
         return e.getText() + " " + e.getChildren().stream().map((c) ->
                 showAllText((Element) c)).collect(Collectors.joining(" "));
