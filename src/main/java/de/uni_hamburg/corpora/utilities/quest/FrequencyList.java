43420bef3cfac2e32f6b8524d9b0327273cf497e
Lange, Dr. Herbert <herbert.lange@uni-hamburg.de>
2022-03-28 17:26:47 +0200

    add comments and reverse order in toString

--- src/main/java/de/uni_hamburg/corpora/utilities/quest/FrequencyList.java
+++ src/main/java/de/uni_hamburg/corpora/utilities/quest/FrequencyList.java
@@ -2,23 +2,46 @@ package de.uni_hamburg.corpora.utilities.quest;
 
 import java.util.*;
 
+/**
+ * Helper class implementing a simple frequency list
+ * @author bba1792, Dr. Herbert Lange
+ * @version 20220328
+ */
 public class FrequencyList {
 
+    // The map storing the list
     private final Map<String, Integer> freqList = new HashMap<>();
 
+    /**
+     * Add a value to the list, increasing its count by 1 if it was already in the list
+     * @param k the new value
+     */
     public void put(String k) {
         freqList.compute(k,(fk, fv) -> (fv == null) ? 1 : fv + 1);
     }
 
+    /**
+     * Adds all values of a collection
+     * @param ks the collection of values
+     */
     public void putAll(Collection<String> ks) {
         for (String k : ks)
             this.put(k);
     }
 
+    /**
+     * Gets the count for a value
+     * @param k the value
+     * @return its count
+     */
     public int get(String k) {
         return freqList.get(k);
     }
 
+    /**
+     * Gets the internal map
+     * @return the map
+     */
     public Map<String,Integer> getMap() {
         return freqList;
     }
@@ -27,7 +50,7 @@ public class FrequencyList {
     public String toString() {
         StringBuilder str = new StringBuilder();
         List<String> keys  = new ArrayList<>(freqList.keySet());
-        keys.sort(Comparator.comparing(freqList::get));
+        keys.sort(Comparator.comparing(freqList::get).reversed());
         for (String k : keys) {
             str.append(k);
             str.append(" - ");
@@ -35,5 +58,13 @@ public class FrequencyList {
             str.append("\n");
         }
         return str.toString();
+    }
+
+    /**
+     * Checks if the list is empty
+     * @return true if empty, false otherwise
+     */
+    public boolean isEmpty() {
+        return freqList.isEmpty();
     }
 }
