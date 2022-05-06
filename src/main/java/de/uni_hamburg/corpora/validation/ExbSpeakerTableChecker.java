3900ded58ca9f897ee9fc5757e4a4602df3fbcc1
Aleksandr Riaposov <bay7303@uni-hamburg.de>
2022-05-06 14:17:38 +0200

    ExbSpeakerTierChecker and ExbSpeakerTableChecker added

---
+++ src/main/java/de/uni_hamburg/corpora/validation/ExbSpeakerTableChecker.java
@@ -0,0 +1,91 @@
+package de.uni_hamburg.corpora.validation;
+
+import de.uni_hamburg.corpora.*;
+import de.uni_hamburg.corpora.EXMARaLDACorpusData;
+
+import de.uni_hamburg.corpora.utilities.TypeConverter;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Collection;
+import java.util.Collections;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.xpath.XPathExpressionException;
+import org.jdom.JDOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ * A class that checks IDs in speaker tables of EXB files correspond to 
+ * respective abbreviations. If there are mismatches, it corrects them.
+ * @author bay7303
+ */
+public class ExbSpeakerTableChecker extends Checker implements CorpusFunction {
+    
+    public ExbSpeakerTableChecker() {
+        super(true);
+    }
+    
+    
+    @Override
+    public Collection<Class<? extends CorpusData>> getIsUsableFor() {
+        return Collections.singleton(EXMARaLDACorpusData.class);
+    }
+    
+    @Override
+    public Report function(CorpusData cd, Boolean fix) throws IOException, SAXException, TransformerException, ParserConfigurationException, XPathExpressionException {
+        Report stats = new Report();
+        Document doc = null;
+        EXMARaLDACorpusData ecd = new EXMARaLDACorpusData();
+        ecd = (EXMARaLDACorpusData) cd;
+        doc = TypeConverter.JdomDocument2W3cDocument(ecd.getJdom());
+        
+        NodeList speakerTable = doc.getElementsByTagName("speaker");
+        
+        for (int i=0; i<speakerTable.getLength(); i++) {
+            Element speakerEl = (Element) speakerTable.item(i);
+            String speakerId = speakerEl.getAttribute("id");
+            NodeList ab = speakerEl.getElementsByTagName("abbreviation");
+            Element abbr = (Element) ab.item(0);
+            String speakerAb = abbr.getTextContent();
+                      
+            if (!speakerAb.equals(speakerId)) {                
+                if (fix) {
+                    speakerEl.setAttribute("id", speakerAb);
+                    String message = "Speaker id was fixed in speaker table, the updated value is " + speakerAb;
+                    stats.addFix(function, cd, message);
+                } else {
+                    String message = "Speaker abbreviation " + speakerAb + " and speaker id " + speakerId + " do not match in the speaker table";
+                    stats.addCritical(function, cd, message);
+                }
+            }
+        }
+        
+        String result = TypeConverter.W3cDocument2String(doc);
+        CorpusIO cio = new CorpusIO();
+        if (fix) {
+            cd.updateUnformattedString(result);
+            cio.write(cd, cd.getURL());
+        }
+        
+        return stats;
+    }
+    
+    @Override
+    public String getDescription() {
+        String description = "A class that checks IDs in speaker tables of EXB files correspond to \n" +
+        "respective abbreviations. If there are mismatches, it corrects them.";
+        return description;
+    }
+    
+    @Override
+    public Report function(Corpus c, Boolean fix) throws SAXException, IOException, ParserConfigurationException, URISyntaxException, JDOMException, TransformerException, XPathExpressionException {
+        Report stats = new Report();
+        for (CorpusData cdata : c.getBasicTranscriptionData()) {
+            stats.merge(function(cdata, fix));
+        }
+        return stats;
+    }
+}
---
+++ src/main/java/de/uni_hamburg/corpora/validation/ExbSpeakerTierChecker.java
@@ -0,0 +1,98 @@
+package de.uni_hamburg.corpora.validation;
+
+import de.uni_hamburg.corpora.*;
+import de.uni_hamburg.corpora.EXMARaLDACorpusData;
+
+import de.uni_hamburg.corpora.utilities.TypeConverter;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.xpath.XPathExpressionException;
+import org.jdom.JDOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ * A class that checks speaker attributes in tiers of EXB files correspond to 
+ * values in the speaker table. For files with only one speaker, it corrects them.
+ * @author bay7303
+ */
+public class ExbSpeakerTierChecker extends Checker implements CorpusFunction {
+    
+    public ExbSpeakerTierChecker() {
+        super(true);
+    }
+    
+    
+    @Override
+    public Collection<Class<? extends CorpusData>> getIsUsableFor() {
+        return Collections.singleton(EXMARaLDACorpusData.class);
+    }
+    
+    @Override
+    public Report function(CorpusData cd, Boolean fix) throws IOException, SAXException, TransformerException, ParserConfigurationException, XPathExpressionException {
+        Report stats = new Report();
+        Document doc = null;
+        EXMARaLDACorpusData ecd = new EXMARaLDACorpusData();
+        ecd = (EXMARaLDACorpusData) cd;
+        doc = TypeConverter.JdomDocument2W3cDocument(ecd.getJdom());
+        
+        NodeList speakerTable = doc.getElementsByTagName("speaker");
+        ArrayList<String> speakerIds = new ArrayList();
+        
+        for (int i=0; i<speakerTable.getLength(); i++) {
+            Element speakerEl = (Element) speakerTable.item(i);
+            String speakerId = speakerEl.getAttribute("id");
+            speakerIds.add(speakerId);
+        }
+        
+        NodeList tierList = doc.getElementsByTagName("tier");
+        
+        for (int i=0; i<tierList.getLength(); i++) {
+            Element tier = (Element) tierList.item(i);
+            String speakerTier = tier.getAttribute("speaker");
+            String tierId = tier.getAttribute("id");
+            if (!speakerIds.contains(speakerTier)) {
+                if (fix && speakerIds.size() == 1 ) {
+                    tier.setAttribute("speaker", speakerIds.get(0));
+                    String message = "Speaker attribute was fixed in tier " + tierId + ", the updated value is " + speakerIds.get(0);
+                    stats.addFix(function, cd, message);                    
+                } else {
+                    String message = "No corresponding id in the speaker table was found for speaker attribute of the tier " + tierId;
+                    stats.addCritical(function, cd, message); 
+                }
+            }
+        }
+        
+        String result = TypeConverter.W3cDocument2String(doc);
+        CorpusIO cio = new CorpusIO();
+        if (fix) {
+            cd.updateUnformattedString(result);
+            cio.write(cd, cd.getURL());
+        }
+        
+        return stats;
+    }
+    
+    @Override
+    public String getDescription() {
+        String description = "A class that checks speaker attributes in tiers of EXB files correspond to \n" +
+        "values in the speaker table. For files with only one speaker, it corrects them.";
+        return description;
+    }
+    
+    @Override
+    public Report function(Corpus c, Boolean fix) throws SAXException, IOException, ParserConfigurationException, URISyntaxException, JDOMException, TransformerException, XPathExpressionException {
+        Report stats = new Report();
+        for (CorpusData cdata : c.getBasicTranscriptionData()) {
+            stats.merge(function(cdata, fix));
+        }
+        return stats;
+    }
+}
