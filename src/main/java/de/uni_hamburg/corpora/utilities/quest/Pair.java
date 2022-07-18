63389943a2e4e43d212807b31ce2a26a3440d3a4
Lange, Dr. Herbert <herbert.lange@uni-hamburg.de>
2022-07-18 10:38:44 +0200

    add toString and equals to Pair

--- src/main/java/de/uni_hamburg/corpora/utilities/quest/Pair.java
+++ src/main/java/de/uni_hamburg/corpora/utilities/quest/Pair.java
@@ -1,5 +1,7 @@
 package de.uni_hamburg.corpora.utilities.quest;
 
+import java.util.Objects;
+
 /**
  * Class implementing a simple pair of values
  * @param <T> the type of the first field
@@ -28,5 +30,26 @@ public class Pair<T,U> {
 
     public void setSecond(U snd) {
         this.snd = snd;
+    }
+
+    @Override
+    public String toString() {
+        return "<" +
+                fst +
+                ", " + snd +
+                '>';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return fst.equals(pair.fst) && snd.equals(pair.snd);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fst, snd);
     }
 }
