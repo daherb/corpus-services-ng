a2e157f7ee34f4500818aa22d0c10cfe7e3b0a41
Herbert Lange <lange@ids-mannheim.de>
2023-02-24 15:22:35 +0100

    move to subpackage

--- src/main/java/de/uni_hamburg/corpora/utilities/publication/ids/InvenioAPITools.java
+++ src/main/java/de/uni_hamburg/corpora/utilities/publication/ids/InvenioAPITools.java
@@ -2,7 +2,7 @@
 * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
 * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
 */
-package de.uni_hamburg.corpora.utilities.publication;
+package de.uni_hamburg.corpora.utilities.publication.ids;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
