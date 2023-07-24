c9258f3d2deff6726ffce32588ef83ba34669cad
Herbert Lange <lange@ids-mannheim.de>
2023-07-24 16:19:57 +0200

    do not copy parent metadata to avoid duplication

--- src/main/java/de/uni_hamburg/corpora/utilities/publication/ids/mapper/MapRecord.java
+++ src/main/java/de/uni_hamburg/corpora/utilities/publication/ids/mapper/MapRecord.java
@@ -95,7 +95,7 @@ public class MapRecord {
         // If we have private files we create a new record for them
         if (!privateFiles.isEmpty()) {
             MapRecord privateRecord = new MapRecord();
-            map.getMetadata().ifPresent(privateRecord::setMetadata);
+            // map.getMetadata().ifPresent(privateRecord::setMetadata);
             map.getTitle().ifPresent((t) -> privateRecord.setTitle(t + " - Private files"));
             privateRecord.setFiles(privateFiles);
             newMap.getRecords().add(privateRecord);
