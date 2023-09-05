71e18ad224ed015b2df9a3f33c0ef851e608533d
Herbert Lange <lange@ids-mannheim.de>
2023-09-05 16:04:55 +0200

    move checker to another package

--- src/main/java/de/idsmannheim/lza/validation/CheckBag.java
+++ src/main/java/de/idsmannheim/lza/validation/CheckBag.java
@@ -2,12 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package de.uni_hamburg.corpora.validation;
+package de.idsmannheim.lza.validation;
 
 import de.uni_hamburg.corpora.Corpus;
 import de.uni_hamburg.corpora.CorpusData;
 import de.uni_hamburg.corpora.CorpusFunction;
 import de.uni_hamburg.corpora.Report;
+import de.uni_hamburg.corpora.validation.Checker;
 import gov.loc.repository.bagit.domain.Bag;
 import gov.loc.repository.bagit.domain.FetchItem;
 import gov.loc.repository.bagit.exceptions.CorruptChecksumException;
