da8e394ea01555aef94e4f97eec027d178688e45
Herbert Lange <github@pietaetskirsche.de>
2024-05-13 12:49:59 +0200

    cleanup imports

--- src/test/java/de/uni_hamburg/corpora/CorpusTest.java
+++ src/test/java/de/uni_hamburg/corpora/CorpusTest.java
@@ -4,11 +4,6 @@ import org.junit.jupiter.api.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Collection;
-import java.util.Map;
 /**
  * Last updated
  * @author Herbert Lange
